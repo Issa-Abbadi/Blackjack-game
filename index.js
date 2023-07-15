93b84c928a2797d39c1f2c65daea9da522f8df85
Issa Abbadi <eisaabbadi1@gmail.com>
2023-07-15 23:39:24 +0300

    adding Start game button

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ let firstCard = 6;
 let secondCard = 7;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
-
 let isAlive = true;
-
 let message = "";
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack! ";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game! ";
-  isAlive = false;
-}
+let messageEl = document.getElementById("message-el");
 
-console.log(message);
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! ";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! ";
+    isAlive = false;
+  }
+
+  console.log(message);
+}
