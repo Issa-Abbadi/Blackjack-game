593d9b093ee7ff72fbb97625dd729bfb9097f005
Issa Abbadi <eisaabbadi1@gmail.com>
2023-07-19 17:39:21 +0300

    adding cards to the sum

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let firstCard = 6;
 let secondCard = 7;
+let cards = [firstCard, secondCard];
+
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -10,8 +12,15 @@ let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
   sumEl.textContent = "Sum: " + sum;
-  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -24,10 +33,14 @@ function startGame() {
   messageEl.textContent = message;
 }
 
+function getRandomCard() {}
+
 function newCard() {
   let card = 7;
 
   sum += card;
 
-  startGame();
+  cards.push(card);
+
+  renderGame();
 }
